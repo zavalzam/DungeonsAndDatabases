b3d36f24d304fd58d5aa5fc6dad58bb0991cfb7d
zavalzam <zavalzam@oregonstate.edu>
2019-12-02 14:41:52 -0800

    Added all the shit

---
+++ src/config.js
@@ -0,0 +1,10 @@
+// TODO: Add your database configuration to this file.
+// NOTE: If you are using source code control (like GitHub), make sure you 
+//       do not check in passwords!
+//
+module.exports = {
+  host: 'classmysql.engr.oregonstate.edu',
+  user: 'cs340_zavalzam',
+  password: 'TempPass123',
+  database: 'cs340_zavalzam'
+};
